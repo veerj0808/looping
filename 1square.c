7e40117cdbfa099b0556dd577b785637fd3c349c
veerj0808 <veerj0808@gmail.com>
2025-10-31 21:56:03 +0530

    Add files via upload

---
+++ 1square.c
@@ -0,0 +1,14 @@
+// to get squares of the given numbers
+
+#include<stdio.h>
+void main()
+{
+	int i,n,f=1;
+	printf("enter the ending number \n");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		f=i*i;
+		printf("square = %d\n",f);
+	}	
+}
